a0daeeecc5cc164c8b80cc611b800ab2f4b12b13
kiulcode <hello.kiulcode@gmail.com>
2023-08-18 22:40:54 -0500

    📕 Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Ruta home";
+});
+
+Route::get('blog', function (){
+   return "Listado de publicaciones";
+});
+
+Route::get('blog/{slug}', function ($slug){
+    return "Datao = $slug";
+});
+
+Route::get('buscar', function (Request $request){
+    return $request->all();
 });
